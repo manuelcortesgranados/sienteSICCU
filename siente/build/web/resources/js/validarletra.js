165b999c550b8c06fefc0042b472cdb469a6bd72
carlosloaiza <carlosloaiza@192.168.1.26>
2013-05-03 09:16:09 -0500

    Borrado carpeta build

--- siente/build/web/resources/js/validarletra.js
+++
@@ -1,7 +0,0 @@
-function validar(e) {
-    tecla = (document.all) ? e.keyCode : e.which;
-    if (tecla==8) return true; //Tecla de retroceso (para poder borrar)
-    patron = /\d/; //Solo acepta números
-    te = String.fromCharCode(tecla);
-    return patron.test(te);
-}
