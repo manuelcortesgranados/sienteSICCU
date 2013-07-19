44a9822aaa44706fe6dcfa1a1c83e2cd6ac34eff
crixguitarr <cdgutierrez6@gmail.com>
2013-07-19 14:01:21 -0500

    Archivos nuevos de plan operativo flujo de caja

---
+++ siente/src/java/cobra/PlanOperativo/FlujoCaja.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cobra.PlanOperativo;
+
+
+
+/**
+ *
+ * @author desarrollo6
+ */
+public class FlujoCaja {
+    ExpenseReport expReport;
+    
+       public ExpenseReport getExpReport() {
+        if (expReport == null) {
+            expReport = new ExpenseReport();
+        }
+        return expReport;
+    }
+ 
+    public void setExpReport(ExpenseReport expReport) {
+        this.expReport = expReport;
+    }
+   
+    
+}
+
+
+ 
+
