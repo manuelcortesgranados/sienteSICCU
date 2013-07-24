8a6eb0ec1bcb0ef0afd2df327e4c18d6a08ac778
desarrollo9 <desarrollo9@desarrollo9-To-be-filled-by-O-E-M>
2013-07-24 09:36:49 -0500

    Arreglo Conflictos gwt y remoto

---
+++ siente/web/resources/js/zopim/zopimfonade.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+window.$zopim || (function(d, s) {
+    var z = $zopim = function(c) {
+        z._.push(c)
+    }, $ = z.s =
+            d.createElement(s), e = d.getElementsByTagName(s)[0];
+    z.set = function(o) {
+        z.set.
+                _.push(o)
+    };
+    z._ = [];
+    z.set._ = [];
+    $.async = !0;
+    $.setAttribute('charset', 'utf-8');
+    $.src = '//cdn.zopim.com/?sqOCI2eogNaEQcRPQyMHb0ChEHAcWpJR';
+    z.t = +new Date;
+    $.
+            type = 'text/javascript';
+    e.parentNode.insertBefore($, e)
+})(document, 'script');
+
+
+
