79535ed90dc95ed8fc43b3f24ffa96d082baf0da
Daniela <Daniela@Desarrollo7>
2013-08-14 18:42:26 -0500

    Crear proyecto botones acciones

---
+++ siente/src/java/co/com/interkont/cobra/planoperativo/client/ModalMacroActividades.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.interkont.cobra.planoperativo.client;
+
+/**
+ *
+ * @author Daniela
+ */
+public class ModalMacroActividades {
+    
+}
---
+++ siente/src/java/co/com/interkont/cobra/planoperativo/client/ModalObjetivos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.interkont.cobra.planoperativo.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ *
+ * @author Daniela
+ */
+public class ModalObjetivos implements IsWidget, EntryPoint {
+
+    @Override
+    public Widget asWidget() {
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onModuleLoad() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
