6710ed560ab8d0a26b4bc1e17e89358d472250ed
desarrollo9 <desarrollo9@desarrollo9-To-be-filled-by-O-E-M>
2013-10-23 12:51:21 -0500

    arreglo a validacion de fechas en contrato

--- siente/src/java/cobra/Supervisor/NuevoContratoBasico.java
+++ siente/src/java/cobra/Supervisor/NuevoContratoBasico.java
@@ -2770,8 +2770,9 @@ public class NuevoContratoBasico implements ILifeCycleAware, Serializable {
                                 }
 
                                 if (total.compareTo(BigDecimal.valueOf(100)) != 0) {
-                                    removerAnticipo();
+                                   removerAnticipo();
                                     if (bundle.getString("fechaformapago").equals("true")) {
+                                        removerAnticipo();
                                         FacesUtils.addErrorMessage("El valor de la suma de los porcentajes(" + total + "%) de las actas parciales difiere del 100%)");
                                         return false;
                                     }
@@ -2982,10 +2983,12 @@ public class NuevoContratoBasico implements ILifeCycleAware, Serializable {
                             return null;
                         }
                         if (!validacionFechasContrato()) {
+                            removerAnticipo();
                             FacesUtils.addErrorMessage(bundle.getString("fechaactaunica"));
                             return null;
                         }
-                        if (!validacionFechasContratoActasyAnticipo()) {
+                        if (!validacionFechasContratoActasyAnticipo()) {   
+                             removerAnticipo();
                             FacesUtils.addErrorMessage(bundle.getString("fechaanticipo"));
                             return null;
                         }
@@ -7273,6 +7276,7 @@ public class NuevoContratoBasico implements ILifeCycleAware, Serializable {
                 case 1:
                     while (i < lisplanifiactapar.size()) {
                         if (fechapagoanticipo.compareTo(lisplanifiactapar.get(i).getDatefechapago()) > 0) {
+                             removerAnticipo();
                             return false;
                         }
                         i++;
