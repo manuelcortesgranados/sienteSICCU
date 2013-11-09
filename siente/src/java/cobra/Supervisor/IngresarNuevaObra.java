cb659e689f510277143b7250dda362c195b095bb
cristian <cristian.gutierrez@interkont.co>
2013-11-09 15:14:47 -0500

    Solucion de la incidencia 918 generar el cronograma en datos basicos del proyecto

--- siente/src/java/cobra/Supervisor/IngresarNuevaObra.java
+++ siente/src/java/cobra/Supervisor/IngresarNuevaObra.java
@@ -4313,6 +4313,8 @@ public class IngresarNuevaObra implements ILifeCycleAware, Serializable {
      */
     public void fnEsProyectoPadre() {
         // si es padre deshabilitrar cronograma
+        disableCronograma = 0;
+        disableAiu = 0;
         if (obranueva.isBooleantienehijos()) {
             disableCronograma = 1;
             disableAiu = 1;
