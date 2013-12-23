8a35d9c987b5bcc7dc258a7747ac4bdd23b83c8d
cristian <cristian.gutierrez@interkont.co>
2013-12-23 11:42:46 -0500

    Arreglo al consultar un contrato hijo desde un convenio

--- siente/src/java/cobra/Supervisor/NuevoContratoBasico.java
+++ siente/src/java/cobra/Supervisor/NuevoContratoBasico.java
@@ -4528,7 +4528,11 @@ public class NuevoContratoBasico implements ILifeCycleAware, Serializable {
     public String detalleContrHijo() {
         Contrato contratoHijo = (Contrato) tablacontconvhijo.getRowData();
         cargarContrato(contratoHijo);
-
+         booltipocontratoconvenio = false;
+        tipoContCon = "Contrato";
+        boolcontrconsultoria = false;
+        getSessionBeanCobra().getCobraService().setAsoContratoCrear(false);
+        limpiarContrato();
         return "consultarContrato";
     }
 
